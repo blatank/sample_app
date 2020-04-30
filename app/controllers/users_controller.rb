d6a2ab81eb6cd336164c3a9e33c80438ce7e6a25
blatank <paddocknote@gmail.com>
2020-04-30 13:03:05 +0000

    Add microposts in users/:id/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   
   # Get /users/:id
   def show
-    @user = User.find(params[:id])
+    @user       = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   # POST /users
