76e5c107b8b4223b6f5aa7bf99cabce25e8ad2fa
blatank <paddocknote@gmail.com>
2020-05-01 05:45:13 +0000

    Implement microposts controll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  
+  private
+    
+    # ここに置かないとMicropostControllerから呼べない
+    # ログインしているかどうかチェック
+    def logged_in_user
+      unless logged_in?
+        # リクエストしていたURL保存
+        store_location
+        
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      # ログインしていたらマイクロポストのインスタンス(空)を取得する
+      @micropost  = current_user.microposts.build 
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
